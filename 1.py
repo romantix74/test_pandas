1b4747d2b68c357e90ec5abcb5339a58c528f7b6
romantix74 <romantix74@list.ru>
2018-08-21 22:32:37 +0300

    sd

--- 1.py
+++ 1.py
@@ -4,13 +4,15 @@ import pandas_datareader.data as web
 
 import datetime
 
-start = datetime.datetime(2010, 1, 1)
-end = datetime.datetime(2013, 1, 27)
+start = datetime.datetime(2018, 1, 1)
+end = datetime.datetime(2018, 1, 27)
 
 #f = web.DataReader('F', 'morningstar', start, end)
 #print f.head()
+#tiker = 'USD000UTSTOM'
+tiker = 'SBERP'
 
-f = web.DataReader('USD000UTSTOM', 'moex', start='2017-07-01', end='2017-07-31')
+f = web.DataReader(tiker, 'moex', start='2018-07-01', end='2018-07-31')
 print f.head()
 
 # import pandas_datareader as pdr

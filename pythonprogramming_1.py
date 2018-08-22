2a7c929da9d455a3dd8e7a7149073e84a9229a8f
romantix74 <romantix74@list.ru>
2018-08-22 21:55:41 +0300

    2

--- pythonprogramming_1.py
+++ pythonprogramming_1.py
@@ -1,5 +1,3 @@
-## python3
-
 import datetime as dt
 import matplotlib.pyplot as plt
 from matplotlib import style
@@ -11,9 +9,15 @@ style.use('ggplot')
 
 start = dt.datetime(2016, 1, 1)
 end = dt.datetime.now()
-df = web.DataReader("TSLA", 'morningstar', start, end)
-df.reset_index(inplace=True)
-df.set_index("Date", inplace=True)
-df = df.drop("Symbol", axis=1)
+df = web.DataReader("USD000UTSTOM", 'moex', start, end)
+# df.reset_index(inplace=True)
+# df.set_index("Date", inplace=True)
+# df = df.drop("Symbol", axis=1)
 
-print(df.head())
+print(df.head(10))
+
+df.to_csv('TSLA.csv')
+df = pd.read_csv('tsla.csv', parse_dates=True, index_col=0)
+#df['Adj Close'].plot()
+df.plot()
+plt.show()
